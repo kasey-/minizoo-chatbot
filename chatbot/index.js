470ae80ba0b1b90c2489fca62c543a50f8931d33
Lucas Fernandez <lucas.fernandez@champ.aero>
2019-02-04 10:59:24 +0100

    Add unit convert support and configuration management

--- chatbot/index.js
+++ chatbot/index.js
@@ -1,33 +1,48 @@
 'use strict';
 
+const yaml = require('js-yaml');
+const fs   = require('fs');
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { WebhookClient } = require('dialogflow-fulfillment');
+const convert = require('convert-units');
 
-const port = 5000;
+let config = {
+  port:5000
+};
+
+try {
+  config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
+} catch (e) {
+  console.log(e);
+}
+
 const app = express();
 
 app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-process.env.DEBUG = 'dialogflow:debug'
+//process.env.DEBUG = 'dialogflow:debug'
 
 app.post('/chatbot/dialogflowFulfillment', (request, response) => {
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+  console.log(`Dialogflow Request headers: ${JSON.stringify(request.headers)}`);
+  console.log(`Dialogflow Request body: ${JSON.stringify(request.body)}`);
 
   const agent = new WebhookClient({ request, response });
   let intentMap = new Map();
 
   function convert(agent) {
-    agent.add('You requested a conversion');
+    console.log('Convert Agent: ');
+    console.log(agent);
+    const conv = convert(1).from('lb').to('kg');
+    agent.add(`You requested a conversion`);
   }
 
   intentMap.set('convert', convert);
   agent.handleRequest(intentMap);
 });
 
-app.listen(port, () =>
-  console.log(`Fullfillment listening on the port ${port}!`),
+app.listen(config.port, () =>
+  console.log(`Fullfillment listening on the port ${config.port}!`),
 );
